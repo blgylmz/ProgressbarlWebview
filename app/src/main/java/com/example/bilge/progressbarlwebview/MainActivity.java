d89e40c7a60d6ba6a6d82c23dbd37028966d5dea
bilge yılmaz <bilgeyilmaz22@gmail.com>
2019-03-21 00:58:22 +0300

    Initial commit

---
+++ app/src/main/java/com/example/bilge/progressbarlwebview/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.bilge.progressbarlwebview;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        WebView vebview = findViewById(R.id.webview);
+        vebview.getSettings().setJavaScriptEnabled(true);
+        vebview.loadUrl("https://www.youtube.com/");
+
+        final ProgressDialog progress = ProgressDialog.show(this,"Youtube","Yükleniyor");
+        progress.show();
+        vebview.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                Toast.makeText(getApplicationContext(), "Sayfa yüklendi", Toast.LENGTH_SHORT).show();
+                progress.dismiss();
+            }
+
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(getApplicationContext(), "Bir hata oluştu", Toast.LENGTH_SHORT).show();
+                progress.dismiss();
+            }
+        });
+
+    }
+}
